301e3e61d923f2632df8db6625ceb01ff534e0ba
Sahil <sahil.ntc@gmail.com>
2021-06-28 13:33:45 +0530

    theory

---
+++ Maths/APGP.py
@@ -0,0 +1,47 @@
+
+def thoery(a,n):
+    '''AP GP
+nth term=> a+(n-1)d
+
+sum= avg n => ((First term +last term)/2)n => n(a+ ((n-1)/2)d)
+
+GP=>
+r= ratio of 2 consecutive term
+
+first=> a
+
+2nd => a*r
+
+nth a *r**(n-1)
+
+sum => a(1-r**n)/(1-r)
+
+mean and median¶
+mean=> all data sum /number of sum
+
+median=> sort and get middle number or get mean of middle two if even count
+
+prime number
+2,3,5,7.....
+
+every prime number can be represented in form 6n +1 or 6n-1 except 2 and 3, where n is a natural number
+
+
+PnC
+Permuatation : arrrangement of R things than can be done out of total N things
+
+np=> fact(n)/fact(n-r)
+
+Combination: selection of R things that can be done out of total N things
+
+nC=> fact(n)/(fact(r)*fact(n-r))
+
+modular arithemtic
+remainder obatained after division operation of two operands is known as modulo operation
+
+%
+
+7%2=1
+
+unsigned long long int =>64 bit if growth of output outgrows this , we use modular arithematic to shorted answer'''
+
