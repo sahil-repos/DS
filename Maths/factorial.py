4b347d3665959425c0348f5b32fe74d33833d644
Sahil <sahil.ntc@gmail.com>
2021-07-07 17:37:12 +0530

    factorial of a number

--- Maths/factorial.py
+++ Maths/factorial.py
@@ -1,5 +1,5 @@
-#factorial of number
-def fact(A):
-  if A==1 or A==0:
-      return 1
-  return A* fact(A-1) 
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
