d0b181f2106d83268667478782cc44c965cb9807
Sahil <sahil.ntc@gmail.com>
2021-07-28 10:17:29 +0530

    code changes

--- Recursion-1/palindromeRec.py
+++ Recursion-1/palindromeRec.py
@@ -5,3 +5,16 @@ def palinRec(s:str,st:int,end:int):
     return (s[st]==s[end]) and (palinRec(s,st+1,end-1))
     
 print(palinRec("1001",0,3))
+
+
+
+def PalinRec2(s:str):
+    if len(s)<=1 :
+        return True
+    else:
+        if(s[0]==s[-1]):
+            return PalinRec2(s[1:-1])
+        else:
+            return False
+print(PalinRec2("RCO-0OCR"))
+    
