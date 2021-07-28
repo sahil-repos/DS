824883aba65d151c0e3d4735397a9d1ad34eceb9
Sahil <sahil.ntc@gmail.com>
2021-07-28 10:10:25 +0530

    code changes

--- Recursion-1/palindromeRec.py
+++ Recursion-1/palindromeRec.py
@@ -2,7 +2,6 @@ def palinRec(s:str,st:int,end:int):
     '''whether a string is palindrome or not'''
     if st>=end:
         return True
-    return (
-        (s[st]==s[end]) and (palinRec(s,st+1,end-1))
-    )
+    return (s[st]==s[end]) and (palinRec(s,st+1,end-1))
+    
 print(palinRec("1001",0,3))
