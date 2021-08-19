a531df50b1aff0c3d5ce7fc2125302b9f70f4e39
sahil-repos <47462984+sahil-repos@users.noreply.github.com>
2021-08-19 17:05:41 +0530

    Implementing function

---
+++ BitWise/chkSetBit.java
@@ -0,0 +1,13 @@
+class CheckBit{
+  public static void main(Sting[] args){
+  //check whether kth bit from left is set or not
+    int n=23,k=3;
+    int y=1;
+    y=y<<(k-1)
+    if ((n & y) ==1){
+    System.out.println("SET");}
+    else{    System.out.println(" NOT SET");}
+
+    
+  }
+}
