e5451765dc5c7d3d9f4d0b1577640b36acea2c55
sahil-repos <47462984+sahil-repos@users.noreply.github.com>
2021-08-19 17:10:33 +0530

    PowerOF2

---
+++ BitWise/chkWhetherNumberIsPowerOF2.java
@@ -0,0 +1,13 @@
+class HelloWorld {
+public static void main(String[] args){
+int n=64;
+ if (n==0)
+   System.out.println("NO");
+  if((n &(n-1)) ==0)
+    System.out.println("YES");
+  else
+     System.out.println("NO");
+
+    
+}
+}
