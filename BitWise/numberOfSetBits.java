ded736c5ccf9a63c2ce1ab043620e33ece97f60f
sahil-repos <47462984+sahil-repos@users.noreply.github.com>
2021-09-24 23:38:34 +0530

    Update numberOfSetBits.java

--- BitWise/numberOfSetBits.java
+++ BitWise/numberOfSetBits.java
@@ -55,15 +55,4 @@ public class Solution {
 }
 
 
-//anothher approach
-// long C = 0;
-// for(int i = 30; i >= 0; i--) {
-// if((A & (1 << i)) > 0) {
-// // bit is set
-// long x = i;
-// x = x * (1 << (i - 1));
-// C += x + 1; // total set bit count up to 2^i
-// A -= 1 << i;
-// C += A; // extra bits
-// }
-// }
+
